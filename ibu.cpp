62768d9b34a798f820469a2af26654be94910adf
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-05-11 14:51:28 +0700

    class ibu

---
+++ ibu.cpp
@@ -0,0 +1,19 @@
+#ifdef IBU_H
+#define IBU_H
+#include <vector>
+
+class ibu {
+    public : 
+        string nama;
+        vector<anak*> daftar_anak;
+
+        ibu(string pNama) :nama(pNama) {
+             cout << "Ibu \"" << nama << "\" ada\n";
+            }
+        ~ibu () {
+            cout << "Ibu \"" << nama << "\" Tidak ada\n"; 
+        }
+
+    void tambahAnaka(anaka*);
+    void cetakAnak();
+};
